b3fd0979cc592789cdd35237ccb1022f12a152d9
Hugo <hugoribeirorneto@gmail.com>
2025-06-02 21:39:43 -0300

    init

---
+++ src/test/java/br/com/hugo/LoginApi/LoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hugo.LoginApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
